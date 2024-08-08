7b78ccffcaab6592c08ce54f6401831ca75f3dfa
Alibek Olimov <98762357+OlimovAlibek@users.noreply.github.com>
2024-08-08 22:49:09 +0500

    Update day9.js

--- day9.js
+++ day9.js
@@ -161,3 +161,68 @@ btn.addEventListener('click', (event) => {
   input.value = ''
   
 })
+
+
+
+/*
+  Exercise 01
+  -----------
+  Add a new row to the table with the details: 'Sean Reyes' (@sreyes)
+*/
+
+const tr = document.createElement('tr')
+const details = ["6", "Sean", "Reyes", "@sreyes"]
+details.forEach(colText => {
+  const col = document.createElement('td')
+  col.innerText = colText
+  tr.appendChild(col)
+})
+
+document.querySelector(".table").appendChild(tr)
+
+
+/*
+  Exercise 02
+  ------------
+  Update Leona Dixon's handle to @dixonl
+*/
+
+// document.querySelector('.table tr:nth-child(4) td:nth-child(4)').innerText = "@dixonl"
+
+
+/*
+  Exercise 03
+  -----------
+  Move Rosa Reed to the top of the table and ensure all the number entries in the first column are updated accordingly.
+*/
+
+// Array.from(document.querySelectorAll('.table tr td:nth-child(1)')).forEach((number, index) => {
+//   number.innerText = index + 1
+// })
+
+
+/*
+  Exercise 04
+  -----------
+  Move the handle column including all of it's rows of contents to be the second column in the table (after the # column)
+*/
+
+Array.from(document.querySelectorAll('.table tr')).forEach(row => {
+  const handles = row.querySelector('th:nth-child(4), td:nth-child(4)')
+  const index = row.querySelector('td:nth-child(1), th:nth-child(1)')
+  index.insertAdjacentElement('afterEnd', handles)
+})
+
+
+/*
+  Exercise 05
+  -----------
+  To make the table easier to read, make every other row on the table to have a darker, shaded colour e.g. #f2f2f2
+*/
+
+Array.from(document.querySelectorAll('.table tr')).forEach((row, index) => {
+  if(index % 2 === 1) {
+    row.style['background-color'] = 'grey'
+  }
+})
+
